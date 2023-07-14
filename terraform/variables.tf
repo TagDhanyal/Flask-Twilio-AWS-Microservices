6d381fb75f38f389ba5037f62c025129365f0c93
Dhanyal Tag <134966284+TagDhanyal@users.noreply.github.com>
2023-07-14 20:09:06 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,11 @@
 variable "access_key" {
   description = "AWS access key"
   type        = string
-  default = "AKIASB2YBC7MCS7KAQBO"
 }
 
 variable "secret_key" {
   description = "AWS secret key"
   type        = string
-  default     = "u3t1QaBDrBhvg4OEGVSA5OKSWVuhGb+ThLK7lZrG"
 }
 
 variable "region" {
